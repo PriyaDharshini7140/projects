0541c3683e77cccedc66aff522ec28ce50579401
priya <riaharshini1998@gmail.com>
2020-12-11 19:44:55 -0500

    save

--- sum_product.js
+++ sum_product.js
@@ -4,12 +4,10 @@ let product=1
 for(let i=0; i<array_1.length;i++)
 {
      sum += array_1[i]
-}
-document.write("sum of the array : ",sum,"<br/>")
-
-for(let i=0; i<array_1.length;i++)
-{
      product *= array_1[i]
 }
+document.write("sum of the array : ",sum,"<br/>")
 document.write("product of the array : ",product,"<br/>")
+
+
 
